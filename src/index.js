b42b80c419a6946ff3bb450f149a5263c7b9ed6b
ahmed-abdi1991 <squarehajji@gmail.com>
2023-10-30 11:59:46 +0300

    assaignment

--- src/index.js
+++ src/index.js
@@ -6,3 +6,20 @@ ReactDOM.render(
   <Form />,
   document.getElementById('root')
 );
+function test(description, testFunction) {
+  return testFunction().then(result => {
+    if (result === true) {
+      console.log(description + " - Passed");
+    } else {
+      console.error(description + " - Failed");
+    }
+  }).catch(error => {
+    console.error(description + " - Failed with error: " + error);
+  });
+}
+
+test("Test passing", () => {
+  return new Promise((resolve, reject) => {
+    resolve(true);
+  });
+});
